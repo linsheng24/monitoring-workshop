5304e370618c5660b3c168aad93423b9540d1590
Jackson <jackson@ya-ya.tw>
2025-03-29 23:31:48 +0800

    init

---
+++ iac/data.tf
@@ -0,0 +1,1 @@
+data "kubernetes_nodes" "all_nodes" {}
---
+++ iac/provider.tf
@@ -0,0 +1,13 @@
+locals {
+  kubeconfig_path = "~/.kube/config"
+}
+
+provider "kubernetes" {
+  config_path = local.kubeconfig_path
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = local.kubeconfig_path
+  }
+}
